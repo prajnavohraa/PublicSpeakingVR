0235593d9d9374f58cd1b47b3966c0b7caf64a38
prajnavohraa <prajna21345@iiitd.ac.in>
2024-04-14 23:22:56 +0530

    added whisper

---
+++ Assets/Samples/OpenAI Unity/0.2.0/Whisper/Whisper.cs
@@ -0,0 +1,139 @@
+﻿using OpenAI;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine.Events;
+
+
+
+
+namespace Samples.Whisper
+{
+    public class Whisper : MonoBehaviour
+    {
+
+        public OnResponseEvent OnResponse;
+        [System.Serializable]
+        public class OnResponseEvent: UnityEvent<string> { }
+
+        [SerializeField] private Button recordButton;
+        [SerializeField] private Image progressBar;
+        [SerializeField] private Text message;
+        [SerializeField] private Dropdown dropdown;
+
+        
+        private readonly string fileName = "output.wav";
+        private readonly int duration = 5;
+        
+        private AudioClip clip;
+        private bool isRecording;
+        private float time;
+        private OpenAIApi openai = new OpenAIApi("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");
+        private List<ChatMessage> messages= new List<ChatMessage>();
+
+        private void Start()
+        {
+            #if UNITY_WEBGL && !UNITY_EDITOR
+            dropdown.options.Add(new Dropdown.OptionData("Microphone not supported on WebGL"));
+            #else
+            foreach (var device in Microphone.devices)
+            {
+                dropdown.options.Add(new Dropdown.OptionData(device));
+            }
+            recordButton.onClick.AddListener(StartRecording);
+            dropdown.onValueChanged.AddListener(ChangeMicrophone);
+            
+            var index = PlayerPrefs.GetInt("user-mic-device-index");
+            dropdown.SetValueWithoutNotify(index);
+            #endif
+        }
+
+        private void ChangeMicrophone(int index)
+        {
+            PlayerPrefs.SetInt("user-mic-device-index", index);
+        }
+        
+        private void StartRecording()
+        {
+            isRecording = true;
+            recordButton.enabled = false;
+
+            var index = PlayerPrefs.GetInt("user-mic-device-index");
+            
+            #if !UNITY_WEBGL
+            clip = Microphone.Start(dropdown.options[index].text, false, duration, 44100);
+            #endif
+        }
+
+        private async void EndRecording()
+        {
+            message.text = "Transcripting...";
+            
+            #if !UNITY_WEBGL
+            Microphone.End(null);
+            #endif
+            
+            byte[] data = SaveWav.Save(fileName, clip);
+            
+            var req = new CreateAudioTranscriptionsRequest
+            {
+                FileData = new FileData() {Data = data, Name = "audio.wav"},
+                // File = Application.persistentDataPath + "/" + fileName,
+                Model = "whisper-1",
+                Language = "en"
+            };
+            var res = await openai.CreateAudioTranscription(req);
+
+            progressBar.fillAmount = 0;
+            message.text = res.Text;
+
+            await AskChatGPT(res.Text);
+
+            recordButton.enabled = true;
+        }
+
+        private void Update()
+        {
+            if (isRecording)
+            {
+                time += Time.deltaTime;
+                progressBar.fillAmount = time / duration;
+                
+                if (time >= duration)
+                {
+                    time = 0;
+                    isRecording = false;
+                    EndRecording();
+                }
+            }
+        }
+
+        public async Task AskChatGPT(string newText){
+            ChatMessage newMessage= new ChatMessage();
+            newMessage.Content=newText;
+            newMessage.Role="user";
+
+            messages.Add(newMessage);
+
+            CreateChatCompletionRequest request = new CreateChatCompletionRequest();
+            request.Messages=messages;
+            request.Model="gpt-3.5-turbo";
+
+            var response= await openai.CreateChatCompletion(request);
+
+            if (response.Choices!=null && response.Choices.Count>0){
+                var chatResponse= response.Choices[0].Message;
+                messages.Add(chatResponse);
+
+                Debug.Log(chatResponse.Content);
+                OnResponse.Invoke(chatResponse.Content);
+            }
+        }
+
+    }
+}
+
+
+
